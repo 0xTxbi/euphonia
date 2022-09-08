63cfde01c392f3e0c91cce91ce8f05a5a2abf7f1
Joseph David <techiejossy@gmail.com>
2022-09-08 22:26:55 +0100

    project setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <p className='text-3xl font-bold bg-blue-500 underline'>index</p>
+    <p className="text-3xl font-bold">Euphonia</p>
   )
 }
 
