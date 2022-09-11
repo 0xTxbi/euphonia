bbe4da0fc90becf28c1ad0e18fdd63526d749a0a
Joseph David <techiejossy@gmail.com>
2022-09-11 13:16:04 +0100

    feat: Set up Prisma

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Sidebar from './Sidebar'
 import { AppShell } from '@mantine/core'
 import Footer from './MusicPlayer'
+import { useState } from 'react';
 
 const PlayerLayout = ({ children }) => {
+    const [opened, setOpened] = useState(false)
+
     return (
 
         <AppShell
+            navbarOffsetBreakpoint='sm'
             navbar={<Sidebar />}
             footer={<Footer />}
         >
